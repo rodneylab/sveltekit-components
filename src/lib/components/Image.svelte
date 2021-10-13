adc1a666294ebbc7f3e97eb930c1584c3b9fa7fb
Rodney Lab <codesign@rodneylab.com>
2021-10-13 10:31:09 +0100

    feat: 📸 add Image component

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let alt: string;
+  export let height: number; // needed to reduce CLS
+  export let src: string;
+  export let sources: { srcset: string; type: string }[];
+  export let placeholder: string;
+  export let width: number; // needed to reduce CLS
+  export let maxWidth: string = '1280px';
+  export let sizes: string = `(max-width: ${maxWidth}) 100vw, ${maxWidth}}`;
+  export let loading: string = 'lazy';
+</script>
+
+<picture>
+  {#each sources as source}
+    <source data-sizes={sizes} data-srcset={source.srcset} type={source.type} {width} {height} />
+  {/each}
+  <img
+    class="lazy"
+    {alt}
+    {loading}
+    decoding="async"
+    {width}
+    {height}
+    data-src={src}
+    src={placeholder}
+    style={`max-width:${maxWidth}`}
+  />
+</picture>
+
+<style>
+  img {
+    height: auto;
+  }
+
+  img:not([src]):not([srcset]) {
+    visibility: hidden;
+  }
+</style>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
 import EmailInputField from '$lib/components/EmailInputField.svelte';
+import Image from '$lib/components/Image.svelte';
 import Map from '$lib/components/Map.svelte';
 import PasswordInputField from '$lib/components/PasswordInputField.svelte';
 import TextArea from '$lib/components/TextArea.svelte';
 import TextInputField from '$lib/components/TextInputField.svelte';
 
-export { EmailInputField, Map, TextArea, PasswordInputField, TextInputField };
+export { EmailInputField, Image, Map, TextArea, PasswordInputField, TextInputField };
