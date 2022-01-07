39cd17cfa914b5e251d61bef92daddce9eac8b5b
Rodney Lab <codesign@rodneylab.com>
2022-01-07 14:40:25 +0000

    style: 🛁 linting

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="@sveltejs/kit" />
+
+export declare global {
+  declare namespace svelte.JSX {
+    interface HTMLProps {
+      importance?: 'auto' | 'high' | 'low' | undefined;
+    }
+  }
+}
