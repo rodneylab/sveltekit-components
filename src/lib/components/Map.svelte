99d8606283735a8f3462bd1b611ffc1640625d7d
Rodney Lab <codesign@rodneylab.com>
2022-01-10 08:58:51 +0000

    fix(map): source leaflet from package rather than cdn

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,14 +1,6 @@
 <script lang="ts">
   import { browser } from '$app/env';
   import website from '$lib/config/website';
-  import type {
-    LatLngExpression,
-    Map,
-    MapOptions,
-    Marker,
-    TileLayer,
-    TileLayerOptions,
-  } from 'leaflet';
   import { onMount } from 'svelte';
 
   export let id: string;
@@ -25,42 +17,34 @@
   const { mapboxAccessToken } = website;
   const { latitude, longitude } = location;
 
-  let leaflet: {
-    map: (element: string | HTMLElement, options?: MapOptions) => Map;
-    marker: (latLong: LatLngExpression) => Marker;
-    tileLayer: (urlTemplate: string, options?: TileLayerOptions) => TileLayer;
-  };
-
-  function setMap() {
+  async function setMap() {
     if (browser) {
-      leaflet = window.L;
-      const map = leaflet.map(id).setView([latitude, longitude], zoom);
-      leaflet
-        .tileLayer(
-          'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}',
-          {
-            attribution:
-              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 19,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: mapboxAccessToken,
-            detectRetina: true,
-          },
-        )
-        .addTo(map);
+      const { map: leafletMap, marker: leafletMarker, tileLayer } = await import('leaflet');
+      const map = leafletMap(id).setView([latitude, longitude], zoom);
+      tileLayer(
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}',
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 19,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: mapboxAccessToken,
+          detectRetina: true,
+        },
+      ).addTo(map);
       if (marker) {
         if (markerMarkup) {
-          leaflet.marker([latitude, longitude]).bindPopup(markerMarkup).addTo(map);
+          leafletMarker([latitude, longitude]).bindPopup(markerMarkup).addTo(map);
         } else {
-          leaflet.marker([latitude, longitude]).addTo(map);
+          leafletMarker([latitude, longitude]).addTo(map);
         }
       }
     }
   }
 
-  onMount(() => {
+  onMount(async () => {
     setMap();
   });
 </script>
@@ -77,12 +61,6 @@
     integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
     crossorigin=""
   />
-  <script
-    src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-    {importance}
-    async
-    crossorigin=""></script>
 </svelte:head>
 
 <div {id} {style} />
