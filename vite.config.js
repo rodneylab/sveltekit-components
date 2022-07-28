3b73d6ae6d0817b97757e65bdd497ad8c3f4649c
Rodney Lab <codesign@rodneylab.com>
2022-07-28 07:58:34 +0100

    fix: 💫 update for SvelteKit breaking changes

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [basicSsl(), sveltekit()],
+};
+
+export default config;
